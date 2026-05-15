8215ea12adef7e31f57e16270116e1df8972e8cd
VOIDPACKET <pincybergwan.224@gmail.com>
2026-05-15 16:12:48 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 #include <string>
+#include <cstdlib> // for std::getenv
+#include <filesystem> // for std::filesystem and std::path etc.
+#include <sstream> // for std::istringstream
+#include <vector>
+
+// what is the preferred delimiter based on what's the preferred separator
+constexpr char path_list_delimiter() {
+	return (std::filesystem::path::preferred_separator == '\\') ? ';' : ':';
+}
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -22,12 +32,40 @@ int main() {
 	  }
 	  
 	  else if (command == "echo") std::cout << "\n";
+
+	  /// Get the paths (std::getenv)
+	  const char* path_value = std::getenv("PATH");
 	  else if (command.find("type ") == 0) {
-		  std::string toPrint = command.substr(5);
-		  if (toPrint == "echo" || toPrint == "exit" || toPrint == "type") {
-			  std::cout << toPrint << " is a shell builtin" << "\n";
+		  std::string toFind = command.substr(5);
+		  
+		  if (toFind == "echo" || toFind == "exit" || toFind == "type") {
+			  std::cout << toFind << " is a shell builtin" << "\n";
 		  } else {
-			  std::cout << toPrint << ": not found" << "\n";
+			  if (path_value) {
+				  
+				  /// Split them and append the wanted file at the 
+				  std::vector(std::string) results;
+				  std::istringstream stream(path_value);
+				  std:string temp_part;
+				  constexpr char delimiter = path_list_delimiter(); // we stored it to avoid repeatedly calling it in the loop
+
+				  while (std::getline(stream, temp_part, delimiter)) {
+					  if (path_list_delimiter == ";") toFind += ".exe";
+					  results.push_back(temp_part + toFind);
+				  }
+
+				  /// Use std::filesystem::exists()
+				  std::string pathToFile;
+				  for (int i = 0; i < results.size(); i++) {
+					  if (std::filesystem:exists(results[i])) {
+						  pathToFile = results[i];
+						  break;
+					  }
+				  }
+				  std::cout << toFind << "is" << pathToFile << "\n";
+			  } 
+			  else std::cout << toFind << ": not found" << "\n";
+			  
 		  }
 	  }
 	  
