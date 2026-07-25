07660738744b67fbfa59c8f1883893f947cf3b70
VOIDPACKET <pincybergwan.224@gmail.com>
2026-07-25 22:49:25 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -150,11 +150,11 @@ std::vector<std::string> handlSingleQuotes(std::string &toPrint) {
 		else if (toPrint[i] == '\\') {
 			// Check bounds BEFORE looking ahead to prevent memory crashes
 			if (i + 1 < toPrint.length()) {
-				if (toPrint[i + 1] == '"') insideSingleQuotes = false;
-				else if (toPrint[i + 1] == '\'') insideDoubleQuotes = false;
+				if (toPrint[i + 1] == '"') insideDoubleQuotes = true;
+				else if (toPrint[i + 1] == '\'') insideSingleQuotes = true;
 
 				currentArgument += toPrint[i + 1];
-				i += 2; // So that we skip the next character (the escaped one)
+				i += 2; // So that we pass the next character (the escaped one)
 			}
 		}
 		else {
